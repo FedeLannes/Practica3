131d209228bbc6b291c1d14a49c021431e643fa4
FedeLannes <matiaslannes64@gmail.com>
2021-09-03 19:27:58 -0300

    Auxilioestoyatrapado

---
+++ github_graphql_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'cb6f0da9e52c2a0e149c';
+const githubClientSecret = '5446f0655033de7d5677a575bdf0e28d0105de22';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
